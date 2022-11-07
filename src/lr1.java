169619318ad757aaf09518daaa009ead061d6a24
Kudean <dkuralenkod@gmail.com>
2022-11-07 12:58:18 +0200

    Initial commit

---
+++ src/lr1.java
@@ -0,0 +1,6 @@
+public class lr1 {
+    public static void main(String[] argv) {
+        System.out.println("Hello world");
+        System.out.println("This task done by KURALENKO DENIS");
+    }
+}
