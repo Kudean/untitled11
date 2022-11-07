60654a986349755f14abf6c7f44cc5bf5a1ce76f
Kudean <dkuralenkod@gmail.com>
2022-11-07 13:06:05 +0200

    Update lr1.java

--- src/lr1.java
+++ src/lr1.java
@@ -2,5 +2,6 @@ public class lr1 {
     public static void main(String[] argv) {
         System.out.println("Hello world");
         System.out.println("This task done by KURALENKO DENIS");
+        System.out.println("lr2, Hello");
     }
 }
